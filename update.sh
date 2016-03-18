529262323f8aec6f98131be5dd2b2c833711d546
Shawn Ma <qsma@thoughtworks.com>
2016-03-18 23:58:41 +0800

    更新编译脚本

--- update.sh
+++ update.sh
@@ -12,13 +12,13 @@ versions=( "${versions[@]%/}" )
 
 for version in "${versions[@]}"; do	
   if [ "${version%%.*}" -ge 3 ]; then
-  	url="http://repo.mongodb.org/apt/ubuntu"
-    fullVersion="$(curl -fsSL "${url}/dists/trusty/mongodb-org/$version/multiverse/binary-amd64/Packages.gz" | gunzip | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg ~ /^mongodb-(org(-unstable)?|10gen)$/ && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
-    fullRepository="${url} trusty/mongodb-org/${version} multiverse"
+  	repoPackage="http://repo.mongodb.org/apt/ubuntu/dists/trusty/mongodb-org/$version/multiverse/binary-amd64/Packages.gz"
+  	fullVersion="$(curl -fsSL "${repoPackage}" | gunzip | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg ~ /^mongodb-(org(-unstable)?|10gen)$/ && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
+  	fullRepository="deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/${version} multiverse"
   else
-  	url="http://downloads-distro.mongodb.org/repo/ubuntu-upstart"
-    fullVersion="$(curl -fsSL "${url}/dists/dist/10gen/binary-amd64/Packages.gz"                             | gunzip | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg ~ /^mongodb-(org(-unstable)?|10gen)$/ && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
-	  fullRepository="${url} dist 10gen"
+  	repoPackage="http://downloads-distro.mongodb.org/repo/ubuntu-upstart/dists/dist/10gen/binary-amd64/Packages.gz"
+  	fullVersion="$(curl -fsSL "${repoPackage}" | gunzip | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg ~ /^mongodb-(org(-unstable)?|10gen)$/ && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
+  	fullRepository="deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen"
 	fi
 	(
 		set -x
